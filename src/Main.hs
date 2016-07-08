059b88fabeda2a9e1b5ccad172bf37719d19604d
Mike Gehard <mike.gehard@gmail.com>
2016-07-08 06:52:38 -0600

    Add a route parameter

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,8 @@
 
 module Main where
 
+import           Data.Monoid ((<>))
+import           Debug.Trace
 import           Web.Scotty
 
 main :: IO ()
@@ -11,12 +13,17 @@ main = do
 
 routes :: ScottyM ()
 routes = do
-    get "/hello" sayHello
+    get "/hello/:name" sayHello
     get "/goodbye" sayGoodBye
 
+-- this returns a 500 if the ':name' is missing
+-- from the route above.
+-- if the :name is there and you don't pass it in (/hello/)
+-- then you get an empty string
 sayHello :: ActionM()
 sayHello = do
-    text "Hello!"
+    name <- param "name"
+    text ("Hello " <> trace (show name) name <> "!")
 
 sayGoodBye :: ActionM ()
 -- you don't need the do if you only have one monadic action
