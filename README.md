41d6bd92bba8dd83a568e945e73eb9023f9c5906
Mike Gehard <mike.gehard@gmail.com>
2016-07-07 20:52:13 -0600

    Simple hello world program

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 Follows:
 
 http://seanhess.github.io/2015/08/04/practical-haskell-getting-started.html
+
+## Pre-requisites
+
+* [Stack](http://docs.haskellstack.org/en/stable/README/)
+
+## Build
+
+`stack build`
+
+## Run: 
+
+`stack exec haskell-api`
--- src/Main.hs
+++ src/Main.hs
@@ -2,4 +2,10 @@ module Main where
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Enter your name: "
+  name <- getLine
+  putStrLn $ greet name
+  putStrLn $ greet "World"
+
+greet :: String -> String
+greet name = "Hello " ++ name ++ "!"
